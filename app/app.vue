22f7b5e50fbb214a8d70ad72b40ce6ecc7ac2c52
Alejandro Garcia <josealegar19@gmail.com>
2026-08-05 00:31:14 -0700

    now splitting the screen between the two elements that will occupy it

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,13 @@
+<script>
+import CRTOverlay from "./components/CRTOverlay.vue";
+</script>
 <template>
-  <div class="crt">[meow]</div>
+  <CRTOverlay>
+    <div style="display: grid; grid-template-columns: 1fr 1fr">
+      <div style="display: flex; flex-direction: row-reverse; padding: 1rem">
+        [MEOW]
+      </div>
+      <div>[BLEH]</div>
+    </div></CRTOverlay
+  >
 </template>
